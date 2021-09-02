e1d20901d6cb8185d2d9750183725f507271d9f6
Isidro Gonzalez <sgonzisidro@gmail.com>
2021-09-02 09:31:32 -0700

    Updating Scraper

--- README.md
+++ README.md
@@ -3,14 +3,21 @@
 This is a simple script for scraping movies data from cinecalidad.
 You decide how you want to save the data.
 
-# Required libraries
+## Required libraries
 - selenium
 - fake_useragent
 - requests
 - pillow
 
-# If want to automitize you can install
+## If want to automitize you can install
 - schedule
-# Required Webdriver
+## Required Webdriver
 - Chrome
 https://chromedriver.chromium.org/
+
+### Desarrollado por
+
+Isidro Gonzalez
+
+<a href="https://www.facebook.com/torocoder">Facebook - Toro coder</a>
+
--- cinecalidad-scraper.py
+++ cinecalidad-scraper.py
@@ -7,6 +7,10 @@ from selenium.webdriver.common.by import By
 import requests
 import io
 from PIL import Image
+import mysql.connector
+
+db = mysql.connector.connect(host="localhost", user="root", passwd="", database="cinecalidad")
+cursor = db.cursor()
 
 options = Options()
 
@@ -18,10 +22,11 @@ options.add_argument(f"user-agent={fake_ua}")
 
 def cinecalidad_crawler():
     driver = webdriver.Chrome("C:/Program Files/chromedriver_win32/chromedriver.exe", options=options)
-    driver.get('https://cinecalidad.ac/')
+    driver.get('https://cinecalidad.ac/') #PAGE
 
     while True:
-        page_links = driver.find_elements_by_xpath('//*[@id="content_inside"]/div/a')
+        page_links = driver.find_elements_by_xpath(
+            '//*[@id="content_inside"]/div[contains(@class, "home_post_cont post_box")]/div/h3/a')
         movie_links = []
 
         for movie_link in page_links:
@@ -30,6 +35,11 @@ def cinecalidad_crawler():
         for link in movie_links:
             driver.get(link)
 
+            title = None
+            poster = None
+            overview = None
+            imdb = None
+            genre = None
             try:
                 title = driver.find_element_by_xpath('//*[@id="main_container"]/div[4]/h1').text
                 print(title)
@@ -56,79 +66,116 @@ def cinecalidad_crawler():
 
                 votes = driver.find_element_by_xpath("//div[@id='imdb-box']").text
                 res = votes.split()
-                imdb = res[3]
+                imdb = res[-4]
                 print(imdb)
 
                 genre = driver.find_element_by_xpath("//strong[text()='Género:']/following-sibling::span/a[1]").text
                 print(genre)
+            except Exception:
+                pass
 
+            video1 = None
+            video2 = None
+            video3 = None
+            video4 = None
+            video5 = None
+            video6 = None
+            video7 = None
+            trailer = None
+            try:
                 online1 = driver.find_element_by_xpath('//*[@id="panel_online"]/ul/a[1]/li')
                 online1.click()
-                WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
-                video1 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
+                WebDriverWait(driver, 10).until(
+                    EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
+                video1 = WebDriverWait(driver, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
                 print(video1)
                 driver.switch_to.default_content()
                 driver.find_element_by_xpath('//*[@id="cambiar_servidor"]').click()
 
                 online2 = driver.find_element_by_xpath('//*[@id="panel_online"]/ul/a[2]/li')
                 online2.click()
-                WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
-                video2 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
+                WebDriverWait(driver, 10).until(
+                    EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
+                video2 = WebDriverWait(driver, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
                 print(video2)
                 driver.switch_to.default_content()
                 driver.find_element_by_xpath('//*[@id="cambiar_servidor"]').click()
 
                 online3 = driver.find_element_by_xpath('//*[@id="panel_online"]/ul/a[3]/li')
                 online3.click()
-                WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
-                video3 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
+                WebDriverWait(driver, 10).until(
+                    EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
+                video3 = WebDriverWait(driver, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
                 print(video3)
                 driver.switch_to.default_content()
                 driver.find_element_by_xpath('//*[@id="cambiar_servidor"]').click()
 
                 online4 = driver.find_element_by_xpath('//*[@id="panel_online"]/ul/a[4]/li')
                 online4.click()
-                WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
-                video4 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
+                WebDriverWait(driver, 10).until(
+                    EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
+                video4 = WebDriverWait(driver, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
                 print(video4)
                 driver.switch_to.default_content()
                 driver.find_element_by_xpath('//*[@id="cambiar_servidor"]').click()
 
                 online5 = driver.find_element_by_xpath('//*[@id="panel_online"]/ul/a[5]/li')
                 online5.click()
-                WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
-                video5 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
+                WebDriverWait(driver, 10).until(
+                    EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
+                video5 = WebDriverWait(driver, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
                 print(video5)
                 driver.switch_to.default_content()
                 driver.find_element_by_xpath('//*[@id="cambiar_servidor"]').click()
 
                 online6 = driver.find_element_by_xpath('//*[@id="panel_online"]/ul/a[6]/li')
                 online6.click()
-                WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
-                video6 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
+                WebDriverWait(driver, 10).until(
+                    EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
+                video6 = WebDriverWait(driver, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
                 print(video6)
                 driver.switch_to.default_content()
                 driver.find_element_by_xpath('//*[@id="cambiar_servidor"]').click()
 
                 online7 = driver.find_element_by_xpath('//*[@id="panel_online"]/ul/a[7]/li')
                 online7.click()
-                WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
-                video7 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
+                WebDriverWait(driver, 10).until(
+                    EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="tviframe"]')))
+                video7 = WebDriverWait(driver, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, '//*[@id="tviframe"]'))).get_attribute('src')
                 print(video7)
                 driver.switch_to.default_content()
                 driver.find_element_by_xpath('//*[@id="cambiar_servidor"]').click()
 
                 trailer = driver.find_element_by_xpath('//*[@id="panel_online"]/ul/a[last()]').get_attribute('data-src')
                 print(trailer)
-
             except Exception:
                 pass
+
+            sql_fetch = f"SELECT * FROM movies WHERE title='{title}'"
+            cursor.execute(sql_fetch)
+            movie = cursor.fetchall()
+
+            if not movie:
+                sql_insert = f"INSERT INTO movies (title, poster, overview, imdb, genre, video1, video2, video3, video4, video5, video6, video7, trailer) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                values = (
+                    title, poster, overview, imdb, genre, video1, video2, video3, video4, video5, video6, video7,
+                    trailer)
+                cursor.execute(sql_insert, values)
+                db.commit()
+
             driver.back()
-
-        pagination = driver.find_element_by_xpath("//a[@class='nextpostslink'][contains(.,'»')]")
-        print(pagination.get_attribute('href'))
-        pagination.click()
-
+        try:
+            pagination = driver.find_element_by_xpath("//a[@class='nextpostslink'][contains(.,'»')]")
+            pagination.click()
+        except Exception:
+            driver.quit()
 
 
 cinecalidad_crawler()
